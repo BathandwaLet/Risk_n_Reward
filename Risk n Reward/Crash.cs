867cca24af04e961ad89daf665f5b1016f7e1b22
Bathandwa <bmap750@gmail.com>
2025-12-14 14:37:06 +0200

    Version 1 of Risk n Reward

---
+++ Risk n Reward/Crash.cs
@@ -0,0 +1,6 @@
+namespace Risk_n_Reward;
+
+public class Crash
+{
+    
+}
---
+++ Risk n Reward/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace Risk_n_Reward;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Risk n' Reward!");
+        Console.Clear();
+        
+        Console.WriteLine("What game do you want to play?");
+        Console.WriteLine("1.Coin Toss \n 2.Black Jack \n 3.Crash \n 4.Pick Five \n 5.Rock, Paper, Scissors");
+        int.TryParse(Console.ReadLine(), out var choiceNumber);
+        string game;
+
+        switch (choiceNumber)
+        {
+            case 1:
+                game = "CoinToss";
+                break;
+            case 2:
+                game = "BlackJack";
+                break;
+            case 3:
+                game = "CrashSimulator";
+                break;
+            case 4:
+                game = "PickFive";
+                break;
+            case 5:
+                game = "RockPaperScissors";
+                break;
+            default: break;
+        }
+        
+        
+    }
+}
+
