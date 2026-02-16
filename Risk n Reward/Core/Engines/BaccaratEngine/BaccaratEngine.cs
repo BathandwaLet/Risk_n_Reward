8bb9b0adfabcdf8c4d84f75036e957b8f794b41e
Bathandwa <bmap750@gmail.com>
2026-02-16 08:38:54 +0200

    feat: BaccaratEngine.cs Added PayoutMultiplier method to handle how much gets paid out dependent on who wins.

--- Risk n Reward/Core/Engines/BaccaratEngine/BaccaratEngine.cs
+++ Risk n Reward/Core/Engines/BaccaratEngine/BaccaratEngine.cs
@@ -8,7 +8,7 @@ namespace Risk_n_Reward.Core.Engines.BaccaratEngine;
 
 public class BaccaratEngine
 {
-    public BaccaratResult Result(List<Card> player, List<Card> dealer, decimal playerBet, BaccaratBetType playerBetType)
+    public BaccaratResult Result(List<Card> player, List<Card> dealer, decimal playerBet, BaccaratBetType betType)
     {
         
         return new  BaccaratResult();
@@ -53,5 +53,17 @@ public class BaccaratEngine
         }
         
         return BaccaratOutcome.Tie;
+    }
+
+    private decimal PayoutMultiplier (BaccaratOutcome gameOutcome, BaccaratBetType betType)
+    {
+        
+        return (gameOutcome, betType) switch
+        {
+            (BaccaratOutcome.PlayerWin, BaccaratBetType.Player) => 2.0m,
+            (BaccaratOutcome.BankerWin, BaccaratBetType.Banker) => 1.95m,
+            (BaccaratOutcome.Tie, BaccaratBetType.Tie) => 8.0m,
+            _ => 0m,
+        };
     }
 }
