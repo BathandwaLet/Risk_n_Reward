3de96c362b78817565d39853129491158f9aa66e
Bathandwa <bmap750@gmail.com>
2026-02-21 12:25:28 +0200

    feat: TexasHoldem.cs - Added basic game ochrestration - Added deck logic  • Generation of a new deck (Shuffling handled within the Cardeck model)  • Dealing of cards to dealer, player and community pile.
     TODO
     call the engine once engine is complete.
     finialise results presentation.

--- Risk n Reward/Games/TexasHoldemPoker/TexasHoldem.cs
+++ Risk n Reward/Games/TexasHoldemPoker/TexasHoldem.cs
@@ -1,4 +1,5 @@
 using Risk_n_Reward.Wallet;
+using Risk_n_Reward.Core.Models.CardDeck;
 
 namespace Risk_n_Reward.Games.TexasHoldemPoker;
 
@@ -6,6 +7,75 @@ public class TexasHoldem : IGame
 {
     public void Start(WalletService wallet)
     {
+        Console.WriteLine("Welcome to Texas Hold 'em Poker!");
+
+        Console.WriteLine($"You currently have {wallet.Balance} VMali.");
+
+        Console.WriteLine("Place your bet:");
+        decimal playerBet;
+        if (!decimal.TryParse(Console.ReadLine(), out playerBet))
+        {
+            throw new ArgumentException("Invalid input!");
+        }
+
+        if (!wallet.PlaceBet(playerBet))
+        {
+            throw new ArgumentException("Insufficient funds!");
+            return;
+        }
         
+        Deck deck = new Deck();
+
+        List<Card> playerHand = new();
+        List<Card> dealerHand = new();
+        List<Card> communityHand = new();
+        
+        Console.WriteLine("Shuffling the deck");
+        Thread.Sleep(1000);
+        Console.Clear();
+        
+        Console.WriteLine("Dealing cards");
+        Thread.Sleep(1000);
+        Console.Clear();
+        
+        //draw the cards
+        //player cards
+        playerHand.Add(deck.Draw());
+        playerHand.Add(deck.Draw());
+        
+        //dealer cards
+        dealerHand.Add(deck.Draw());
+        dealerHand.Add(deck.Draw());
+        
+        //community pile
+        for (int i = 0; i < 4; i++)
+        {
+            communityHand.Add(deck.Draw());
+        }
+        
+        //prints *card* *card* for dealer
+        Console.WriteLine("Card Card");
+        
+        //prints community hand
+        Console.Write("\nCommunity Pile: ");
+        foreach (Card card in communityHand)
+        {
+            Console.Write(card + " ");
+        }
+        
+        //prints player hand
+        Console.Write("\nPlayer Hand: ");
+        foreach (Card card in playerHand)
+        {
+            Console.Write(card + " ");
+        }
+        
+        // call engine
+        
+        //results presentation and wallet update
+        
+        //wallet updated, show balance.
+        Console.WriteLine($"Your new balance is: {wallet.Balance} VMali");
+        Console.ReadKey();
     }
 }
