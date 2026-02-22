29d68355c72537894e45f6b710001dea74eb7d35
Bathandwa <bmap750@gmail.com>
2026-02-22 20:17:27 +0200

    feat(TexasHoldemEngine.cs): Implement helper methods in holdem engine.
    -Added AllCards method to concate the hand with the community cards before parsing the IsX() methods.
    -Added GetFlushCards method which returns the cards that form part of a flush.
    
    Note: IsRoyalFlush, IsStraightFlush, etc. methods are stubbed
    and will be completed in a follow-up commits before testing.

--- Risk n Reward/Core/Engines/TexasHoldemEngine/TexasHoldemEngine.cs
+++ Risk n Reward/Core/Engines/TexasHoldemEngine/TexasHoldemEngine.cs
@@ -41,8 +41,8 @@ public class TexasHoldemEngine
         }
 
         //if the player and dealer have the same hand type we evaluate the hand values
-        var playerHandValue = HandValue(playerHand);
-        var dealerHandValue  = HandValue(dealerHand);
+        var playerHandValue = HandValue(playerHand,communityCards);
+        var dealerHandValue  = HandValue(dealerHand, communityCards);
 
         if (playerHandValue > dealerHandValue)
         {
@@ -77,64 +77,107 @@ public class TexasHoldemEngine
     private bool IsRoyalFlush(List<Card> hand, List<Card> community)
     {
         //An ace high straight Flush.
+        var cards = AllCards(hand, community);
+        if (IsFlush(hand, community))
+        {
+            
+        }
+        
         return false;
     }
     //Check Straight Flush
     private bool IsStraightFlush(List<Card> hand, List<Card> community)
     {
         //Five consecutive cards in the same suit.
+        var cards = AllCards(hand, community);
+        if (IsFlush(hand, community))
+        {
+            var flushCards = GetFlushCards(hand, community);
+            
+            if(IsStraight(flushCards, new List<Card>()))
+            {
+                return true;
+            }
+        }
+        
         return false;
     }
     //Check Four of a Kind
     private bool IsFourOfAKind(List<Card> hand, List<Card> community)
     {
         //Four cards of the same rank.
+        var cards = AllCards(hand, community);
         return false;
     }
     //Check Full House
     private bool IsFullHouse(List<Card> hand, List<Card> community)
     {
         //Three cards of the same rank with two cards of another same rank
+        var cards = AllCards(hand, community);
         return false;
     }
     //Check Flush
     private bool IsFlush(List<Card> hand, List<Card> community)
     {
         //Five cards in the same suit (not consecutive).
+        var cards = AllCards(hand, community);
         return false;
     }
     //Check Straight
     private bool IsStraight(List<Card> hand, List<Card> community)
     {
         //Five consecutive cards of different suits. (Aces can count either as a high or a low card.)
+        var cards = AllCards(hand, community);
         return false;
     }
     //Check Three of a Kind
     private bool IsThreeOfAKind(List<Card> hand, List<Card> community)
     {
         //Three cards of the same rank.
+        var cards = AllCards(hand, community);
         return false;
     }
     //Check Two Pair
     private bool IsTwoPair(List<Card> hand, List<Card> community)
     {
         //Two cards of the same rank together with two cards of another same rank.
+        var cards = AllCards(hand, community);
         return false;
     }
     //Check Pair
     private bool IsPair(List<Card> hand, List<Card> community)
     {
         //Two cards of the same rank.
+        var cards = AllCards(hand, community);
         return false;
     }
     //Else High Card
-    private int HandValue(List<Card> hand)
+    private int HandValue(List<Card> hand, List<Card> community)
     {
         int sum = 0;
-        // calculations...
+        // calculations of the highest five cards...
+        var cards = AllCards(hand, community);
         return sum;
     }
+    
+    // helper functions to combine the cards(hands + community). 
+    private List<Card> AllCards(List<Card> hand, List<Card> communityCards)
+    {
+        return hand.Concat(communityCards).ToList();
+    }
 
+    //method to return the cards that make up a flush
+    private List<Card> GetFlushCards(List<Card> hand, List<Card> community)
+    {
+        var cards = AllCards(hand, community);
+
+        var suitGroup = cards.GroupBy(c => c.Suit).FirstOrDefault(g =>
+            g.Count() <= 5);
+        
+        return suitGroup.ToList();
+    }
+
+    //method that returns the appropriate payout multiplier
     private decimal PayoutMultiplier(GameResult gameResult, THHandType player)
     {
         //win cases
