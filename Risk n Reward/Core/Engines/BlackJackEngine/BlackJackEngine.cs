1733319653d18adf801f5313ba2028c21e9c463c
Bathandwa <bmap750@gmail.com>
2026-03-10 13:45:59 +0200

    feat: Implemented BlackJack engine to handle all game logic. feat: Implemented CardShuffle method. feat: PrintHand method added to print hands. feat: Added InitialDrawCards method for the initial draw for the first cards. !feat: Removed ParsePlayerChoice.
    chore: ShowDealerInitialHand now used in engine.
    chore: CalculateHandValue now used in engine.
    chore: CardShuffle now used in engine.
    
    refactor: refactored Result method.

--- Risk n Reward/Core/Engines/BlackJackEngine/BlackJackEngine.cs
+++ Risk n Reward/Core/Engines/BlackJackEngine/BlackJackEngine.cs
@@ -6,12 +6,9 @@ namespace Risk_n_Reward.Core.Engines.BlackJackEngine;
 
 public class BlackJackEngine
 {
-    public BlackJackResult Result(List<Card> player, List<Card> computer, decimal playerBet)
+    public BlackJackResult Result(List<Card> player, List<Card> computer, Deck deck)
     {
-        int playerValue = CalculateHandValue(player);
-        int computerValue = CalculateHandValue(computer);
-        BlackJackOutcome gameResult = GameResult(playerValue, computerValue);
-        
+        BlackJackOutcome gameResult = PlayGame(player, computer, deck);
         decimal payout = Payout(gameResult);
         
         return new BlackJackResult 
@@ -21,6 +18,88 @@ public class BlackJackEngine
         };
     }
 
+    /*
+     * int index = 2;
+       string playerChoice = "";
+       
+       while (CalculateHandValue(playerHand) < 21 && playerChoice != "S")
+       {
+           Console.WriteLine("Press H to hit, S to stand.");
+           playerChoice = Console.ReadLine().ToUpper();
+           
+           if (playerChoice == "H")
+           {
+               playerHand.Add(deck.Draw());
+
+               for (int i = 0; i <= index; i++)
+               {
+                   Console.Write(playerHand[i] + " ");
+               }
+               Console.WriteLine(CalculateHandValue(playerHand));
+               Console.WriteLine();
+
+               index++;
+           }
+           else
+           {
+               Console.WriteLine("Invalid input");
+           }
+       }
+     */
+    public BlackJackOutcome PlayGame(List<Card> player, List<Card> computer, Deck deck)
+    {
+        CardShuffle();
+        player = InitialDrawCards(player,deck);
+        computer = InitialDrawCards(computer, deck);
+        
+        ShowDealerInitialHand(computer);
+        PrintHand(player,1);
+        
+        if (Console.KeyAvailable)
+        {
+            var playerKey = Console.ReadKey(true).Key;
+            
+            while (CalculateHandValue(player) <= 21 && playerKey != ConsoleKey.S)
+            {
+                
+                if (CalculateHandValue(player) > 21)
+                {
+                    return BlackJackOutcome.Lose;
+                }
+
+                Console.WriteLine("Press H to hit, S to stand.");
+                
+                if (playerKey == ConsoleKey.H)
+                {
+                    player.Add(deck.Draw());
+                    PrintHand(player, 1);
+                }
+            
+                if (playerKey == ConsoleKey.S)
+                {
+                    PrintHand(player,1);
+                    PrintHand(computer,2);
+                    
+                    if (CalculateHandValue(computer) < 17)
+                    {
+                        computer.Add(deck.Draw());
+                        PrintHand(computer, 2);
+                    }
+
+                    if (CalculateHandValue(computer) > 21)
+                    {
+                        return BlackJackOutcome.Win;
+                    }
+                    
+                    return GameResult(CalculateHandValue(player), CalculateHandValue(computer));
+                }
+            
+            }
+        }
+
+        return BlackJackOutcome.Push;
+    }
+    
     private BlackJackOutcome GameResult(int player, int computer)
     {
         if (player > 21) 
@@ -48,7 +127,7 @@ public class BlackJackEngine
     {
         if (outcome == BlackJackOutcome.Win)
         {
-            return 1.5m;
+            return 2m;
         }
         else if (outcome == BlackJackOutcome.Push)
         {
@@ -71,5 +150,59 @@ public class BlackJackEngine
 
         return total;
     }
+
+    private void PrintHand(List<Card> Cards, int playerOrComputer)
+    {
+        if (playerOrComputer == 1)
+        {
+            Console.WriteLine("\nThe player cards: ");
+            foreach (var card in Cards)
+            {
+                Console.Write(card + " ");
+            }
+            Console.WriteLine(" " + CalculateHandValue(Cards));
+        }
+
+        if (playerOrComputer == 2)
+        {
+            Console.WriteLine("\nThe dealer cards: ");
+            foreach (var card in Cards)
+            {
+                Console.Write(card + " ");
+            }
+            Console.WriteLine(" " + CalculateHandValue(Cards));
+        } 
+        
+    }
     
+    public void ShowDealerInitialHand(List<Card> dealerHand)
+    {
+        Console.WriteLine("\nDealer's Hand:");
+        Console.WriteLine(dealerHand[0]);
+
+        int visibleValue = (int)dealerHand[0].Rank;
+        Console.WriteLine($"Dealer shows: {visibleValue}");
+
+    }
+
+    private List<Card> InitialDrawCards(List<Card>cards, Deck deck)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            cards.Add(deck.Draw());
+        }
+
+        return cards;
+    }
+    
+    private static void CardShuffle()
+    {
+        Console.WriteLine("Shuffling the deck");
+        Thread.Sleep(1000);
+        Console.Clear();
+        
+        Console.WriteLine("Dealing cards");
+        Thread.Sleep(1000);
+        Console.Clear();
+    }
 }
--- Risk n Reward/Games/BlackJack/BlackJack.cs
+++ Risk n Reward/Games/BlackJack/BlackJack.cs
@@ -1,3 +1,5 @@
+using Risk_n_Reward.Core.Engines.BlackJackEngine;
+using Risk_n_Reward.Core.Models.BlackJackModels.Outcomes;
 using Risk_n_Reward.Core.Models.CardDeck;
 using Risk_n_Reward.Wallet;
 using static Risk_n_Reward.Core.Engines.BlackJackEngine.BlackJackEngine;
@@ -21,79 +23,20 @@ public class BlackJack : IGame
         List<Card> playerHand = new();
         List<Card> dealerHand = new();
 
+        var engine = new BlackJackEngine();
+        var result = engine.Result(playerHand,dealerHand,deck);
 
-        playerHand.Add(deck.Draw());
-        dealerHand.Add(deck.Draw());
-        playerHand.Add(deck.Draw());
-        dealerHand.Add(deck.Draw());
-
-        Console.Clear();
-        ShowDealerInitialHand(dealerHand);
-        Console.WriteLine($"Your current hand {playerHand[0]} {playerHand[1]} \n {CalculateHandValue(playerHand)}");
-
-        int index = 2;
-        string playerChoice = "";
-        
-        while (CalculateHandValue(playerHand) < 21 && playerChoice != "S")
+        switch (result.Outcome)
         {
-            Console.WriteLine("Press H to hit, S to stand.");
-            playerChoice = Console.ReadLine().ToUpper();
-            
-            if (playerChoice == "H")
-            {
-                playerHand.Add(deck.Draw());
-
-                for (int i = 0; i <= index; i++)
-                {
-                    Console.Write(playerHand[i] + " ");
-                }
-                Console.WriteLine(CalculateHandValue(playerHand));
-                Console.WriteLine();
-
-                index++;
-            }
-            else
-            {
-                Console.WriteLine("Invalid input");
-            }
-        }
-
-        if (CalculateHandValue(playerHand) > 21)
-        {
-            Console.WriteLine("BUST! \n You lose!");
-            Console.WriteLine($"Your new balance is {wallet.Balance} VMali");
-            return;
-        }
-
-        Console.WriteLine($"Dealer's hand {dealerHand[0]} {dealerHand[1]} {CalculateHandValue(dealerHand)}");
-        
-        while (CalculateHandValue(dealerHand) < 17)
-        {
-            dealerHand.Add(deck.Draw());
-        }
-
-        if (CalculateHandValue(dealerHand) > 21)
-        {
-            Console.WriteLine("YOU WIN!");
-            wallet.Payout(playerBet * 1.5m);
-            Console.WriteLine($"Your new balance is {wallet.Balance} VMali");
-        }
-        else if (CalculateHandValue(playerHand) > CalculateHandValue(dealerHand))
-        {
-            Console.WriteLine("YOU WIN!");
-            wallet.Payout(playerBet * 1.5m);
-            Console.WriteLine($"Your new balance is {wallet.Balance} VMali");
-        }
-        else if (CalculateHandValue(playerHand) == CalculateHandValue(dealerHand))
-        {
-            Console.WriteLine("PUSH");
-            wallet.Payout(playerBet);
-            Console.WriteLine($"Your new balance is {wallet.Balance} VMali");
-        }
-        else
-        {
-            Console.WriteLine("You Lose");
-            Console.WriteLine($"Your new balance is {wallet.Balance} VMali");
+            case BlackJackOutcome.Win:
+                WinMessage(playerBet,result.PayoutMultiplier ,wallet);
+                break;
+            case BlackJackOutcome.Push:
+                PushMessage(playerBet, wallet);
+                break;
+            case BlackJackOutcome.Lose:
+                LoseMessage(wallet);
+                break;
         }
 
     }
@@ -136,36 +79,24 @@ public class BlackJack : IGame
         }
         
     }
-    
-    void ShowDealerInitialHand(List<Card> dealerHand)
-    {
-        Console.WriteLine("\nDealer's Hand:");
-        Console.WriteLine(dealerHand[0]);
 
-        int visibleValue = (int)dealerHand[0].Rank;
-        Console.WriteLine($"Dealer shows: {visibleValue}");
-
-    }
-    
-    private static BJ ParsePlayerChoice(string input)
+    void WinMessage(decimal playerBet,decimal payoutMultiplier, WalletService wallet)
     {
-        return input switch
-        {
-            "H" => BJ.Hit ,
-            "S" => BJ.Stand,
-            _ => BJ.Null,
-        };
+        Console.WriteLine("YOU WIN!");
+        wallet.Payout(playerBet * payoutMultiplier);
+        Console.WriteLine($"Your new balance is {wallet.Balance} VMali");
     }
 
-    private static void CardShuffle()
+    void PushMessage(decimal playerBet, WalletService wallet)
     {
-        Console.WriteLine("Shuffling the deck");
-        Thread.Sleep(1000);
-        Console.Clear();
-        
-        Console.WriteLine("Dealing cards");
-        Thread.Sleep(1000);
-        Console.Clear();
+        Console.WriteLine("PUSH");
+        wallet.Payout(playerBet);
+        Console.WriteLine($"Your new balance is {wallet.Balance} VMali");
     }
-    
+
+    void LoseMessage(WalletService wallet)
+    {
+        Console.WriteLine("You Lose");
+        Console.WriteLine($"Your new balance is {wallet.Balance} VMali");
+    }
 }
