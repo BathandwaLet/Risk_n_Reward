c4ebd363df06652d025d69da297f848997b5e5e8
Bathandwa <bmap750@gmail.com>
2026-05-19 09:42:45 +0200

    refactor: fixed method name from computer to Computer.

--- Risk n Reward/Games/CoinToss/ComputerToss.cs
+++ Risk n Reward/Games/CoinToss/ComputerToss.cs
@@ -4,7 +4,7 @@ namespace Risk_n_Reward.Games.CoinToss;
 
 public class ComputerToss
 {
-    public static CoinSide computer()
+    public static CoinSide Computer()
     {
         Random rnd = new Random();
         CoinSide  computerChoice =  (rnd.Next(0, 2) == 0)? CoinSide.H : CoinSide.T;
