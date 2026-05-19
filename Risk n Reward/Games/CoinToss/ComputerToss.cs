d9d82347881dff4f1073d54edaa764b56eeccc1c
Bathandwa <bmap750@gmail.com>
2026-05-19 10:01:08 +0200

    refactored: CoinSide enum H to Heads and T to Tails.

--- Risk n Reward/Games/CoinToss/ComputerToss.cs
+++ Risk n Reward/Games/CoinToss/ComputerToss.cs
@@ -7,7 +7,7 @@ public class ComputerToss
     public static CoinSide Computer()
     {
         Random rnd = new Random();
-        CoinSide  computerChoice =  (rnd.Next(0, 2) == 0)? CoinSide.H : CoinSide.T;
+        CoinSide  computerChoice =  (rnd.Next(0, 2) == 0)? CoinSide.Heads : CoinSide.Tails;
 
         return computerChoice;
     }
