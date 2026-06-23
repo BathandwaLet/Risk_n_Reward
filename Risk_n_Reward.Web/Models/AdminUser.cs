9d7a71e63b663d9e89c5e2077961905ac862269d
Bathandwa <bmap750@gmail.com>
2026-06-23 10:04:35 +0200

    feat: Define the AdminUser Model.

---
+++ Risk_n_Reward.Web/Models/AdminUser.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Risk_n_Reward.Web.Models;
+
+public class AdminUser
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public Guid PublicId { get; set; } = Guid.NewGuid();
+    
+    [Required]
+    [MaxLength(50)]
+    public string UserName { get; set; } = string.Empty;
+    
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+    
+    public AdminRole Role { get; set; }
+    
+    public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+    
+    public DateTime LastActiveDate { get; set; } = DateTime.UtcNow;
+}
