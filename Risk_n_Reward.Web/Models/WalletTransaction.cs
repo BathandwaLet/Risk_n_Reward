e7b68c6de931c18aa8eb6c9003567be87f86c0c6
Bathandwa <bmap750@gmail.com>
2026-06-24 09:41:02 +0200

    feat: Added WalletTransaction model.

--- Risk_n_Reward.Web/Models/WalletTransaction.cs
+++ Risk_n_Reward.Web/Models/WalletTransaction.cs
@@ -5,5 +5,24 @@ namespace Risk_n_Reward.Web.Models;
 
 public class WalletTransaction
 {
+    [Key]
+    public int Id { get; set; }
     
+    [Required]
+    public int PlayerId { get; set; }
+    
+    public TransactionType Type { get; set; }
+    
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Amount { get; set; }
+    
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal BalanceAfter { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [ForeignKey(nameof(PlayerId))] 
+    public Player Player { get; set; } = null!;
 }
