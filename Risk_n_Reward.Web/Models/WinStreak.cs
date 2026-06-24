a41ac1b6ad7398d7ce28223f05515e2627d6a2d4
Bathandwa <bmap750@gmail.com>
2026-06-24 10:12:07 +0200

    feat: Added winstreak model

--- Risk_n_Reward.Web/Models/WinStreak.cs
+++ Risk_n_Reward.Web/Models/WinStreak.cs
@@ -5,5 +5,23 @@ namespace Risk_n_Reward.Web.Models;
 
 public class WinStreak
 {
+    [Key]
+    public int Id { get; set; }
     
-}
+    [Required]
+    public int PlayerId { get; set; }
+    
+    [Required]
+    public int GameId { get; set; }
+
+    public int CurrentStreak { get; set; } = 0;
+
+    public int BestStreak { get; set; } = 0;
+    
+    public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+    
+    [ForeignKey(nameof(PlayerId))]
+    public Player Player { get; set; } = null!;
+    
+    [ForeignKey(nameof(GameId))]
+    public Game Game { get; set;} = null!;
