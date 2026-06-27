274ec411dcb3c258e6f554eb2bfad44991efa6dd
Bathandwa <bmap750@gmail.com>
2026-06-27 18:01:08 +0200

    feat: Added imports for AspNetCore MVC, EntityFrameworkCore, Models, Core (Class Library) and Data.
    feat: Initial scaffolding for CoinToss controller.
    
    feat Implemented private field for DbContext.

---
+++ Risk_n_Reward.Web/Controllers/CoinTossController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Risk_n_Reward.Web.Models;
+using Risk_n_Reward.Core;
+using Risk_n_Reward.Web.Data;
+
+namespace Risk_n_Reward.Web.Controllers;
+
+public class CoinTossController : Controller
+{
+    private readonly ApplicationDbContext _context;
+}
