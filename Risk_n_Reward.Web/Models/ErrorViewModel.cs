bffc4cb38eca33ff45b4092080eefa579e94ef9e
Bathandwa <bmap750@gmail.com>
2026-06-27 17:24:52 +0200

    chore: Commit web app files

---
+++ Risk_n_Reward.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Risk_n_Reward.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
