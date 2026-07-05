c1f4448a5bce9eb7b9772a0d6c88bf702382699b
Bathandwa <bmap750@gmail.com>
2026-07-05 20:53:22 +0200

    fix: MultiplierHub now inherits for Microsoft.AspNetCore.SignalR.Hub.

--- Risk_n_Reward.Web/Hub/MultiplierHub.cs
+++ Risk_n_Reward.Web/Hub/MultiplierHub.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.SignalR;
+using Risk_n_Reward.Web.Controllers;
 
 namespace Risk_n_Reward.Web.Hub;
 
-public class MultiplierHub
+public class MultiplierHub : Microsoft.AspNetCore.SignalR.Hub
 {
-    /*public async Task Multiplier()
+    /*public async Task Multiplier(string cashOut, decimal crashPoint)
     {
         
     }*/
