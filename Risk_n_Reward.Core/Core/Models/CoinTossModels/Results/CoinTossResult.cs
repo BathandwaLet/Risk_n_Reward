a3f79cc75ae9a968a625f5626c1f035aae0b0335
Bathandwa <bmap750@gmail.com>
2026-07-07 10:34:31 +0200

    fix: Added accessor to Win and Payout fields.
    feat: Added Computer and Player Fields to save the player selections and the computer generated outcome.

--- Risk_n_Reward.Core/Core/Models/CoinTossModels/Results/CoinTossResult.cs
+++ Risk_n_Reward.Core/Core/Models/CoinTossModels/Results/CoinTossResult.cs
@@ -1,7 +1,11 @@
+using Risk_n_Reward.Core.Core.Models.CoinTossModels.CoinSide;
+
 namespace Risk_n_Reward.Core.Models.CoinTossModels.Results;
 
 public class CoinTossResult
 { 
-    public bool Win;
-    public decimal Payout;
+    public bool Win  { get; init; }
+    public decimal Payout  { get; init; }
+    public CoinSide Computer { get; init; }
+    public CoinSide Player { get; init; }
 }
