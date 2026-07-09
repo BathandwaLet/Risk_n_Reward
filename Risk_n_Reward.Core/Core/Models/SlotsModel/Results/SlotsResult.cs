9b74e3c5adf2754596ccabdbfab67242651d11c4
Bathandwa <bmap750@gmail.com>
2026-07-09 09:47:28 +0200

    fix: Removed unnecessary Result field.

--- Risk_n_Reward.Core/Core/Models/SlotsModel/Results/SlotsResult.cs
+++ Risk_n_Reward.Core/Core/Models/SlotsModel/Results/SlotsResult.cs
@@ -6,7 +6,6 @@ namespace Risk_n_Reward.Core.Models.SlotsModel.Results;
 public class SlotsResult
 {
     public SlotsSymbols[] ReelsOutcome { get; init; }
-    public SlotsOutcome Result { get; init; } 
     public decimal PayoutMultiplier { get; init; }
     public bool IsWin => PayoutMultiplier > 0m;
 }
