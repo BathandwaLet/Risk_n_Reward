3906fbc7f88de8350286c2da8ec033a2135a5637
Bathandwa <bmap750@gmail.com>
2026-07-13 14:53:52 +0200

    fix(convert-to): changed the parameter to type string and used that as an argument in the return switch expression.

--- Risk_n_Reward.Web/Models/SlotToImage.cs
+++ Risk_n_Reward.Web/Models/SlotToImage.cs
@@ -4,20 +4,18 @@ namespace Risk_n_Reward.Web.Models;
 
 public class SlotToImage
 {
-    public static string ConvertToImage(SlotsSymbols slotsSymbol)
+    public static string ConvertToImage(string slotsSymbol)
     {
-        string slotName = slotsSymbol.ToString();
-        
-        return slotName switch
+        return slotsSymbol switch
         {
-            ("Cherry") => "~/images/gameassets/slots/cherries.png",
-            ("ThreeCherries") => "~/images/gameassets/slots/three-cherries.png",
-            ("Seven") => "~/images/gameassets/slots/seven.png",
-            ("Clover") => "~/images/gameassets/slots/clover.png",
-            ("Lemon") => "~/images/gameassets/slots/lemon.png",
-            ("Bell") => "~/images/gameassets/slots/bell.png",
-            ("Wild") => "~/images/gameassets/slots/wild.png",
-            _ => "~/images/gameassets/errors/general/unknown.png"
+            ("Cherry") => "images/gameassets/slots/cherries.png",
+            ("ThreeCherries") => "images/gameassets/slots/three-cherries.png",
+            ("Seven") => "images/gameassets/slots/seven.png",
+            ("Clover") => "images/gameassets/slots/clover.png",
+            ("Lemon") => "images/gameassets/slots/lemon.png",
+            ("Bell") => "images/gameassets/slots/bell.png",
+            ("Wild") => "images/gameassets/slots/wild.png",
+            _ => "images/gameassets/errors/general/error-404.png"
         };
     }
 }
