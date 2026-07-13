1cb6b5343d66119b5ad5e0d60deb2ea12f7bc406
Bathandwa <bmap750@gmail.com>
2026-07-13 14:09:46 +0200

    feat: Added slot to image class the convert the enum to an appropriate img path.

--- Risk_n_Reward.Web/Models/SlotToImage.cs
+++ Risk_n_Reward.Web/Models/SlotToImage.cs
@@ -1,6 +1,23 @@
+using Risk_n_Reward.Core.Models.SlotsModel.Symbols;
+
 namespace Risk_n_Reward.Web.Models;
 
 public class SlotToImage
 {
-    
+    public static string ConvertToImage(SlotsSymbols slotsSymbol)
+    {
+        string slotName = slotsSymbol.ToString();
+        
+        return slotName switch
+        {
+            ("Cherry") => "~/images/gameassets/slots/cherries.png",
+            ("ThreeCherries") => "~/images/gameassets/slots/three-cherries.png",
+            ("Seven") => "~/images/gameassets/slots/seven.png",
+            ("Clover") => "~/images/gameassets/slots/clover.png",
+            ("Lemon") => "~/images/gameassets/slots/lemon.png",
+            ("Bell") => "~/images/gameassets/slots/bell.png",
+            ("Wild") => "~/images/gameassets/slots/wild.png",
+            _ => "~/images/gameassets/errors/general/unknown.png"
+        };
+    }
 }
